ac8176e5754faf0a0e5a0db147c5ae04a8168a58
https://github.com/Yared-Worku <yaredow8343@gmail.com>
2026-09-03 21:26:38 +0300

    authservice

--- auth-frontend/lib/features/auth/services/auth_service.dart
+++ auth-frontend/lib/features/auth/services/auth_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../../../core/config/app_config.dart';
+import '../../../core/config/env_config.dart';
 import '../models/user.dart';
 
 class AuthService {
